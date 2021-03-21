17a2117d1f2cf4f209072024dd997dc857253722
Soltani Rami <33840147+soltanirami@users.noreply.github.com>
2021-03-21 17:47:26 +0100

    generating the application using the Spring Initializr
    Generating the application using the Spring Initializr with these dependencies:
    - Lombok
    - H2 Database
    - Spring Web
    - Spring Data JPA

---
+++ src/main/java/fr/sg/bankaccount/MyBankAccountApplication.java
@@ -0,0 +1,13 @@
+package fr.sg.bankaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBankAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBankAccountApplication.class, args);
+	}
+
+}
