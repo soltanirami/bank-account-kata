416cab2f64e0b94875d8d8768aae1822086663cc
Soltani Rami <33840147+soltanirami@users.noreply.github.com>
2024-12-04 10:01:47 +0100

    Update share.java

--- share.java
+++ share.java
@@ -1,1 +1,10 @@
 
+SELECT 
+    relname AS table_name,
+    pg_size_pretty(pg_total_relation_size(relid)) AS total_size,
+    pg_size_pretty(pg_relation_size(relid)) AS table_size,
+    pg_size_pretty(pg_indexes_size(relid)) AS indexes_size,
+    pg_size_pretty(pg_total_relation_size(relid) - pg_relation_size(relid) - pg_indexes_size(relid)) AS toast_size
+FROM pg_catalog.pg_statio_user_tables
+ORDER BY pg_total_relation_size(relid) DESC
+LIMIT 10;
